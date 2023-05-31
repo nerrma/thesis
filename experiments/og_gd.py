e50f4714ad675fbe501af6e1e19a56c79fc0152b
Sina <sina.alsharif@gmail.com>
2023-05-31 17:52:39 +1000

    fix file name, IACV has a more convincing behaviour

--- experiments/og_gd.py
+++ experiments/og_gd.py
@@ -3,8 +3,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-np.random.seed(4389)
-
 
 def sample_from_logreg(p=20, n=500):
     X = np.random.normal(0, 1, size=(n, p))  # sample (n, p) from standard normal
@@ -48,10 +46,11 @@ def hess_F(X, y, theta, lbd=1e-6):
 def run_sim(n, p, n_iter=250):
     X, theta_s, y = sample_from_logreg(p=p, n=n)
 
-    # theta = np.random.normal(0, 1, size=(p,))
+    lbd_v = 1e-6 * n
+
     theta = np.zeros(p)
     alpha = 0.5 / n
-    alpha_t = 0.5 / n
+    alpha_t = alpha
 
     theta_cv = np.zeros((n, p))
     theta_true = np.zeros((n, p))
@@ -66,16 +65,19 @@ def run_sim(n, p, n_iter=250):
     }
 
     for t in range(0, n_iter):
-        f_grad = nabla_F(X, y, theta)
-        f_hess = hess_F(X, y, theta)
-        theta = theta - alpha * f_grad
+        f_grad = nabla_F(X, y, theta, lbd=lbd_v)
+        f_hess = hess_F(X, y, theta, lbd=lbd_v)
         # print(f"loss : {F(X, y, theta)}")
 
         for i in range(0, n):
-            hess_minus_i = f_hess - hess_F(X[i].reshape(1, -1), [y[i]], theta)
-            grad_minus_i = f_grad - nabla_F(X[i].reshape(1, -1), [y[i]], theta)
+            hess_minus_i = f_hess - hess_F(
+                X[i].reshape(1, -1), [y[i]], theta, lbd=lbd_v
+            )
 
-            # TODO, FIX IACV STEP
+            grad_minus_i = f_grad - nabla_F(
+                X[i].reshape(1, -1), [y[i]], theta, lbd=lbd_v
+            )
+
             theta_cv[i] = (
                 theta_cv[i]
                 - alpha_t * grad_minus_i
@@ -83,15 +85,22 @@ def run_sim(n, p, n_iter=250):
             )
 
             theta_true[i] = theta_true[i] - alpha * nabla_F(
-                np.delete(X, (i), axis=0), np.delete(y, (i), axis=0), theta_true[i]
+                np.delete(X, (i), axis=0),
+                np.delete(y, (i), axis=0),
+                theta_true[i],
+                lbd=lbd_v,
             )
 
             theta_ns[i] = theta + np.linalg.inv(hess_minus_i) @ nabla_F(
-                X[i].reshape(1, -1), [y[i]], theta
+                X[i].reshape(1, -1), [y[i]], theta, lbd=lbd_v
             )
+
             theta_ij[i] = theta + np.linalg.inv(f_hess) @ nabla_F(
-                X[i].reshape(1, -1), [y[i]], theta
+                X[i].reshape(1, -1), [y[i]], theta, lbd=lbd_v
             )
+
+        # actually update theta
+        theta = theta - alpha * f_grad
 
         err_approx["IACV"][t] = np.mean(
             np.linalg.norm(theta_cv - theta_true, 2, axis=1)
@@ -99,4 +108,5 @@ def run_sim(n, p, n_iter=250):
         err_approx["NS"][t] = np.mean(np.linalg.norm(theta_ns - theta_true, 2, axis=1))
         err_approx["IJ"][t] = np.mean(np.linalg.norm(theta_ij - theta_true, 2, axis=1))
         err_approx["hat"][t] = np.mean(np.linalg.norm(theta - theta_true, 2, axis=1))
+
     return err_approx
