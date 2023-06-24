8e4ebf32fa17f8b8dbddcd830122646e5fe7e118
Sina <sina.alsharif@gmail.com>
2023-06-24 23:17:46 +1000

    jax running FAST, issue was in index removal

--- experiments/jax_gd.py
+++ experiments/jax_gd.py
@@ -2,7 +2,8 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from jax import vmap, jacrev, jacfwd, grad, jit, pmap
+from jax import vmap, jacrev, jacfwd, grad, jit, pmap, profiler
+from functools import partial
 import jax.numpy as jnp
 from sampler import sample_from_logreg
 
@@ -15,37 +16,8 @@ def pi(theta):
     return jnp.linalg.norm(theta, 2)
 
 
-@jit
 def F_mod(theta, X, y, lbd):
     return jnp.sum(l(X, y, theta)) + lbd * pi(theta)
-
-
-@jit
-def update(
-    theta,
-    theta_cv,
-    theta_ns,
-    theta_ij,
-    grad_Z,
-    grad_minus,
-    hess_minus,
-    f_hess,
-    alpha_t=1e4,
-):
-    vmap_matmul = jit(vmap(jnp.matmul, in_axes=(0, 0)))
-
-    theta_cv = (
-        theta_cv
-        - alpha_t * grad_minus
-        - alpha_t * vmap_matmul(hess_minus, (theta_cv - theta))
-    )
-
-    theta_ns = theta + vmap_matmul(jnp.linalg.pinv(hess_minus), grad_Z)
-    theta_ij = theta + vmap(jnp.matmul, in_axes=(None, 0))(
-        jnp.linalg.pinv(f_hess), grad_Z
-    )
-
-    return (theta_cv, theta_ns, theta_ij)
 
 
 def run_sim(n, p, n_iter=250):
@@ -53,14 +25,14 @@ def run_sim(n, p, n_iter=250):
 
     lbd_v = 1e-6 * n
 
-    theta = jnp.ones(p)
+    theta = jnp.zeros(p)
     alpha = 0.5 / n
     alpha_t = alpha
 
-    theta_cv = jnp.ones((n, p))
-    theta_true = [jnp.ones(p)] * n
-    theta_ns = jnp.ones((n, p))
-    theta_ij = jnp.ones((n, p))
+    theta_cv = jnp.zeros((n, p))
+    theta_true = [jnp.zeros(p)] * n
+    theta_ns = jnp.zeros((n, p))
+    theta_ij = jnp.zeros((n, p))
 
     err_approx = {
         "IACV": np.zeros(n_iter),
@@ -72,32 +44,19 @@ def run_sim(n, p, n_iter=250):
     nabla_F = jit(grad(F_mod))
     hess_F = jit(jacfwd(jacrev(F_mod)))
 
-    mask = ~jnp.diag(jnp.ones(n, dtype=bool))
     grad_Z_f = jit(vmap(nabla_F, in_axes=(None, 0, 0, None)))
     hess_Z_f = jit(vmap(hess_F, in_axes=(None, 0, 0, None)))
     vmap_matmul = jit(vmap(jnp.matmul, in_axes=(0, 0)))
 
     for t in range(0, n_iter):
-        f_grad = nabla_F(theta, X, y, lbd_v)
-        f_hess = jnp.nan_to_num(hess_F(theta, X, y, lbd_v))
+        f_grad = jnp.nan_to_num(nabla_F(theta, X, y, lbd_v), nan=lbd_v)
+        f_hess = jnp.nan_to_num(hess_F(theta, X, y, lbd_v), nan=lbd_v)
 
-        grad_Z = grad_Z_f(theta, X, y, lbd_v)
-        hess_Z = hess_Z_f(theta, X, y, lbd_v)
+        grad_Z = jnp.nan_to_num(grad_Z_f(theta, X, y, lbd_v), nan=lbd_v)
+        hess_Z = jnp.nan_to_num(hess_Z_f(theta, X, y, lbd_v), nan=lbd_v)
 
         grad_minus = f_grad - grad_Z
         hess_minus = f_hess - hess_Z
-
-        # theta_cv, theta_ns, theta_ij = update(
-        #    theta,
-        #    theta_cv,
-        #    theta_ns,
-        #    theta_ij,
-        #    grad_Z,
-        #    grad_minus,
-        #    hess_minus,
-        #    f_hess,
-        #    alpha_t=alpha_t,
-        # )
 
         theta_cv = (
             theta_cv
@@ -105,14 +64,23 @@ def run_sim(n, p, n_iter=250):
             - alpha_t * vmap_matmul(hess_minus, (theta_cv - theta))
         )
 
-        theta_ns = theta + vmap_matmul(jnp.linalg.pinv(hess_minus), grad_Z)
-        theta_ij = theta + vmap(jnp.matmul, in_axes=(None, 0))(
-            jnp.linalg.pinv(f_hess), grad_Z
+        theta_ns = theta + jnp.nan_to_num(
+            vmap_matmul(jnp.linalg.inv(hess_minus), grad_Z), nan=lbd_v
+        )
+        theta_ij = theta + jnp.nan_to_num(
+            jit(vmap(jnp.matmul, in_axes=(None, 0)))(jnp.linalg.inv(f_hess), grad_Z),
+            nan=lbd_v,
         )
 
         for i in range(n):
-            theta_true[i] = theta_true[i] - alpha * nabla_F(
-                theta_true[i], X[mask[i, :]], y[mask[i, :]], lbd_v
+            theta_true[i] = theta_true[i] - alpha * jnp.nan_to_num(
+                nabla_F(
+                    theta_true[i],
+                    np.delete(X, (i), axis=0),
+                    np.delete(y, (i), axis=0),
+                    lbd=lbd_v,
+                ),
+                nan=lbd_v,
             )
 
         # actually update theta
